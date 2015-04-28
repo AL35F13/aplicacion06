375a99e834a16268eaa9891bec656cfd54b6ca94
AL35F13 <anahi2028@gmail.com>
2015-04-28 13:21:03 -0500

    dos
    agregaron botones: Barrer y Eventos

--- js/acciones.js
+++ js/acciones.js
@@ -3,6 +3,8 @@ $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 $('#btnvibrar').on('tap',function(){navigator.vibrate(1000);});//tap a btnvibrar	
 $('#btnbeep').on('tap', function(){navigator.beep(1);});
+$('#izquierda').on('swipeleft', function(){alert("Barrio a la izquierda");});//swip izquierda
+$('#derecha').on('swiperight', function(){alert("Barrio a la derecha");});//swip derecha
 }); 
 });
 
