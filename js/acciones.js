8e744415fa7a32c718c25b623e8664d9799a3d40
AL35F13 <anahi2028@gmail.com>
2015-04-29 14:47:16 -0500

    cuatro
    cambiar beep

--- js/acciones.js
+++ js/acciones.js
@@ -2,7 +2,7 @@
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 $('#btnvibrar').on('tap',function(){navigator.vibrate(1000);});//tap a btnvibrar	
-$('#btnbeep').on('tap', function(){navigator.beep(1);});
+$('#btnbeep').on('tap', function(){navigator.notification.beep(1)});
 $('#izquierda').on('swipeleft', function(){alert("Barrio a la izquierda");});//swip izquierda
 $('#derecha').on('swiperight', function(){alert("Barrio a la derecha");});//swip derecha
 }); 
