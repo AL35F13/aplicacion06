d1b91ec19ef59b1def139fec257d64ff5114ecf5
AL35F13 <anahi2028@gmail.com>
2015-05-06 13:54:21 -0500

    cinco
    se agrego pagina Listado de eventos

--- js/acciones.js
+++ js/acciones.js
@@ -5,6 +5,15 @@ $('#btnvibrar').on('tap',function(){navigator.vibrate(1000);});//tap a btnvibrar
 $('#btnbeep').on('tap', function(){navigator.notification.beep(1)});
 $('#izquierda').on('swipeleft', function(){alert("Barrio a la izquierda");});//swip izquierda
 $('#derecha').on('swiperight', function(){alert("Barrio a la derecha");});//swip derecha
+document.addEventListener("pause", function(){
+	$('#listado').append("<p> La aplicacion se pauso </p>");
+	});
+document.addEventListener("resume", function(){
+	$('#listado').append("<p>Se reinicio la aplicacion </p>");
+	});	
+$(window).on('orientationchange', function(e){
+	$('#listado').append("<p>Orientacion: " + e.orientation + "</p>");
+	});
 }); 
 });
 
